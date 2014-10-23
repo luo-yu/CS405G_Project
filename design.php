3e2fc2aacedcb8af38bb53b4e2aa286fd0457d89
Tristan Lankford <tlankford88@gmail.com>
2014-10-23 18:39:33 -0400

    test comment

---
+++ design.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>Santa's Blackmarket Test Home-page 0.0</title>
+    :
+    <link type="text/css" rel="stylesheet" href="405projectStyling.css" />
+</head>
+
+
+<body>
+
+    <div class="centeredHeader">
+
+        <p>Welcome to the Toy Store</p>
+        <p id="HeaderText   ">Register</p>
+        <p id="HeaderText   ">Log-In</p>
+        <p id="HeaderText   ">Shop</p>
+        <p id="HeaderText   ">Checkout</p>
+
+    </div>
+</body>
+</html>
