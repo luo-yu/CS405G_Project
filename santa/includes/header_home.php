19f83782936becbc503418b7fee15e80a2c35c8d
Yu Luo <luoy6@miamioh.edu>
2014-11-23 22:07:23 -0500

    Working search

--- santa/includes/header_home.php
+++ santa/includes/header_home.php
@@ -35,12 +35,10 @@
 	<div class="content">
 	
 	<div class="search left">
-		<form name="search" method="get" action="search.php" id="search">
-			<input type="text" value="keywords" name="keyword" class="s0" />
+			<form name="search" method="get" action="search.php" id="search" enctype="multipart/form-data">
+			<input type="text" value="" name="user_query" class="s0" />
 			<br />
-			<select name="title" class="s2">
-			<option value="00000019">Toys</option><option value="00000044"> Games</option></select>
-			<br />
+			
 			<input type="submit" name="search" value="Search Products" class="button marT5" />
 			<input type="hidden" name="page" value="search" />
 		</form>
--- santa/search.php
+++ santa/search.php
@@ -1,3 +1,4 @@
+<?php include '../includes/db_connection.php'; ?>
 <!DOCTYPE HTML>
 <html>
 <head>
@@ -33,134 +34,59 @@
 		
 	<div class="content">
 
-<div id="leftnav">
-	<div class="search">
-
-		<form name="search" method="get" action="search.php" id="search">
-			<input type="text" value="keywords" name="keyword" class="s0" />
-			<br />
-			<select name="title" class="s2">
-			<option value="00000019"> Toys</option><option value="00000034">Games</option></select>
-			<br />
-			<input type="submit" name="search" value="Search Products" class="button marT5" />
-			<input type="hidden" name="page" value="search" />
-		</form>
-		<br /><br />
-		
-		<h3>About Us</h3><br/>
-		<p class="width180">How Are you doing  <a href="about.php">Read More >> </a></p>
-	</div>
-</div><!-- leftnav -->
-
-
 <div id="rightnav">
 
 	<div class="product-list">
-		<h2>Our Products</h2>
-		<a class="pages" href="products.php">&lt;prev</a>
-		&nbsp;|&nbsp;
-		<a class="pages" href="products.php">next&gt;</a>
-			<ul>
-				<li>
-					<div class="image">
-						<a href="detail.php">
-						<img src="images/167_2835774.scale_20.JPG" alt=" Ambrosia Salad" width="190" height="130"/>
-						</a>
-					</div>
-					<div class="detail">
-						<p class="name"><a href="detail.php"> Ambrosia Salad</a></p>
-						<p class="view"><a href="detail.php">purchase</a> | <a href="detail.php">view details >></a></p>
-					</div>
-				</li>
-				<li>
-					<div class="image">
-						<a href="detail.php">
-						<img src="images/167_2835774.scale_20.JPG" alt=" Apple Pie a la Mode" width="190" height="130"/>
-						</a>
-					</div>
-					<div class="detail">
-						<p class="name"><a href="detail.php"> Apple Pie a la Mode</a></p>
-						<p class="view"><a href="detail.php">purchase</a> | <a href="detail.php">view details >></a></p>
-					</div>
-				</li>
-				<li>
-					<div class="image">
-						<a href="detail.php">
-						<img src="images/430_3151480.scale_20.JPG" alt=" Apple Turnover" width="190" height="130"/>
-						</a>
-					</div>
-					<div class="detail">
-						<p class="name"><a href="detail.php"> Apple Turnover</a></p>
-						<p class="view"><a href="detail.php">purchase</a> | <a href="detail.php">view details >></a></p>
-					</div>
-				</li>
-				<li>
-					<div class="image">
-						<a href="detail.php">
-						<img src="images/430_3150132.scale_20.JPG" alt=" Baked Alaska" width="190" height="130"/>
-						</a>
-					</div>
-					<div class="detail">
-						<p class="name"><a href="detail.php"> Baked Alaska</a></p>
-						<p class="view"><a href="detail.php">purchase</a> | <a href="detail.php">view details >></a></p>
-					</div>
-				</li>
-				<li>
-					<div class="image">
-						<a href="detail.php">
-						<img src="images/700_3473780.scale_20.JPG" alt=" Baklava" width="190" height="130"/>
-						</a>
-					</div>
-					<div class="detail">
-						<p class="name"><a href="detail.php"> Baklava</a></p>
-						<p class="view"><a href="detail.php">purchase</a> | <a href="detail.php">view details >></a></p>
-					</div>
-				</li>
-				<li>
-					<div class="image">
-						<a href="detail.php">
-						<img src="images/430_3151480.scale_20.JPG" alt=" Banana Bread" width="190" height="130"/>
-						</a>
-					</div>
-					<div class="detail">
-						<p class="name"><a href="detail.php"> Banana Bread</a></p>
-						<p class="view"><a href="detail.php">purchase</a> | <a href="detail.php">view details >></a></p>
-					</div>
-				</li>
-				<li>
-					<div class="image">
-						<a href="detail.php">
-						<img src="images/430_3150132.scale_20.JPG" alt=" Banana Pudding" width="190" height="130"/>
-						</a>
-					</div>
-					<div class="detail">
-						<p class="name"><a href="detail.php"> Banana Pudding</a></p>
-						<p class="view"><a href="detail.php">purchase</a> | <a href="detail.php">view details >></a></p>
-					</div>
-				</li>
-				<li>
-					<div class="image">
-						<a href="detail.php">
-						<img src="images/167_2835774.scale_20.JPG" alt=" Banana Split" width="190" height="130"/>
-						</a>
-					</div>
-					<div class="detail">
-						<p class="name"><a href="detail.php"> Banana Split</a></p>
-						<p class="view"><a href="detail.php">purchase</a> | <a href="detail.php">view details >></a></p>
-					</div>
-				</li>
-				<li>
-					<div class="image">
-						<a href="detail.php">
-						<img src="images/167_2835774.scale_20.JPG" alt=" Black Forest Cake" width="190" height="130"/>
-						</a>
-					</div>
-					<div class="detail">
-						<p class="name"><a href="detail.php"> Black Forest Cake</a></p>
-						<p class="view"><a href="detail.php">purchase</a> | <a href="detail.php">view details >></a></p>
-					</div>
-				</li>
-		</ul>
+		
+		<!-- item box -->
+	<div id="item_box">
+  		<?php 
+		
+		if(isset($_GET['search'])){
+		
+		$search_query=$_GET['user_query'];
+		
+		
+		$get_pro="
+		SELECT * 
+		FROM items WHERE item_name like '%$search_query%'";
+		//executing the query
+		$run_pro=mysqli_query($connection, $get_pro);
+		
+		
+		while($row_pro= mysqli_fetch_array($run_pro)){
+			$item_id = $row_pro['item_id'];
+			$item_name = $row_pro['item_name'];
+			$item_category = $row_pro['item_category'];
+			$item_price = $row_pro['item_price'];
+			$item_image = $row_pro['item_image'];
+		
+			echo "
+				<div id='single_item'>
+					
+						<h3>$item_name</h3>
+						
+						<img src='http://cs.uky.edu/~ylu236/santa/item_images/$item_image' width ='180' height ='180' />
+						
+						<p><b> $$item_price </b></p>
+						
+						<a href='details.php?pro_id=$item_id' style ='float:left;'>Details</a>
+						
+						
+						<a href='index.php?pro_id=$item_id'><button style='float:right'/>Add to Cart</a>
+				
+				</div>
+			
+			
+			";
+	}//end while
+		
+	}//end if
+		?>
+
+	 </div><!--end item box -->
+		
+		
 	</div><!-- product-list -->
 	
 	
