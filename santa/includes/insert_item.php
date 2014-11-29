ba092e53e159fd347588f47f7bc71f5b39b3349f
Yu Luo <luoy6@miamioh.edu>
2014-11-29 10:59:11 -0500

    View Orders is working

---
+++ santa/includes/insert_item.php
@@ -0,0 +1,155 @@
+<?php require_once("../../includes/db_connection.php"); ?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Santa's Black Market| Home</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name ="description" content ="Sweets Complete">
+<meta name="keywords" content="">
+<link rel="stylesheet" href="../css/main.css" type="text/css">
+<link rel="shortcut icon" href="../images/favicon.ico?v=2" type="image/x-icon" />
+</head>
+<body>
+<div id="wrapper">
+	<div id="maincontent">		
+	<div id="header">
+		<div id="logo" class="left">
+			<a href="../index.php"><img src="../images/logo.png" alt="SweetsComplete.Com"/></a>
+		</div>
+		<div class="right marT10">
+			<b>
+			<a href="../login.php" >Login</a> |<a href="../members.php" >Our Members</a> |<a href="../cart.php" >Shopping Cart</a>
+			</b>
+		</div>
+		<ul class="topmenu">
+		<li><a href="../index.php">Home</a></li>
+		<li><a href="../products.php">Products</a></li>
+		
+		</ul>
+		<br>
+		<div class="banner"><p></p></div>
+		<br class="clear"/>
+	</div> <!-- header -->
+		
+	<div class="content">
+		
+	<div class="product-list">
+		
+		
+		
+		<!--Script for the text area box-->
+<script src="//tinymce.cachefly.net/4.1/tinymce.min.js"></script>
+<script>tinymce.init({selector:'textarea'});</script>
+
+<form action="insert_item.php" method="post" enctype="multipart/form-data">
+	
+		<table align="center" width="750" border="2" bgcolor="FFCCFF">
+			
+			<!-- Insert new Items by admins-->
+			<tr align="center">
+				<td colspan ="7"><h2>Insert New Item Here</h2></td>
+			</tr>
+			
+			
+			<tr>
+				<td align="right"><b>Item Name: </b> </td>
+				<td><input type="text" name="item_name" size ="60" required/></td>
+			</tr>
+			
+			<tr>
+				<td align="right"><b>Category: </b> </td>
+				<td><select name="item_category">
+					<option value="">- Select -</option>
+					<option value="Toys">Toys</option>
+					<option value="Games">Games</option>
+				</select></td>
+				
+				
+
+			</tr>
+			
+			<tr>
+				<td align="right"><b>Item image: </b> </td>
+				<td><input type="file" name="item_image" required /></td>
+			</tr>
+			
+			<tr>
+				<td align="right"><b>Price:</b> </td>
+				<td><input type="text" name="item_price" required /></td>
+			</tr>
+			
+			<tr>
+				<td align="right"><b>Description:</b> </td>
+				<td><textarea name="item_description" cols="20" rows="10"></textarea></td>
+			</tr>
+			
+			
+			
+			<tr align ="center">
+				<td colspan="7"><input type="submit" name="insert_post" value ="Insert Item Now" /></td>
+			</tr>
+				
+		</table>
+	</form>
+
+
+
+		
+
+
+<?php
+
+	//Logic to insert item to database
+
+	/* If the submit (a insert) button is clicked
+	*/
+	if(isset($_POST['insert_post'])){
+		
+		/*Getting the text data from the fields*/
+		$item_name = $_POST['item_name'];
+		$item_category = $_POST['item_category'];
+		
+		$item_price = $_POST['item_price'];
+		$item_description = $_POST['item_description'];
+		
+		/*Getting the image from the field*/
+		$item_image = $_FILES['item_image']['name'];
+		$item_image_tmp=$_FILES['item_image']['tmp_name'];
+		
+		/*Upload the file to the admin_area's item_images folder*/
+		move_uploaded_file($item_image_tmp, "http://www.cs.uky.edu/~ylu236/santa/item_images/$item_image");
+		
+		/*Insert the new information into the database*/
+		$insert_item = "insert into items (item_name,item_category,item_price,item_description,item_image) values ('$item_name','$item_category','$item_price','$item_description','$item_image')";	
+		
+		$insert_pro=mysqli_query($connection, $insert_item);
+		
+		if($insert_pro){
+			echo "<script>alert('Item Has Been Inserted')</script>";
+			echo "<script>window.open('insert_item.php','_self')</script>";
+		}
+		
+	}
+?>		
+		
+		
+		
+		
+		
+		
+		
+	</div><!-- product-list -->
+	
+	<br class="clear-all"/>
+</div><!-- content -->
+	
+	</div><!-- maincontent -->
+
+	<div id="footer">
+	
+	</div><!-- footer -->
+	
+</div><!-- wrapper -->
+
+</body>
+</html>
---
+++ santa/staff/view_orders.php
@@ -0,0 +1,60 @@
+<?php
+
+// This file allows the administrator to view every order.
+
+
+// Set the page title and include the header:
+$page_title = 'View All Orders';
+
+// The header file begins the session.
+
+// Require the database connection:
+//require(MYSQL);
+require("../../includes/db_connection.php");
+echo '<h3>View Orders</h3><table border="0" width="100%" cellspacing="4" cellpadding="4" id="orders">
+<thead>
+	<tr>
+    <th align="center">Order ID</th>
+	<th align="center">Item Name</th>
+	<th align="center">Quantity</th>
+    <th align="center">Total</th>
+    <th align="right">Customer Name</th>
+  
+    <th align="center">Shipping Address</th>
+	<th align="center">Billing Address</th>
+  
+  </tr></thead>
+<tbody>';
+
+// Make the query:
+$q = 'SELECT I.item_name, C.quantity, O.order_id, O.total_price,U.name,O.order_date, O.shipping_address, O.billing_address FROM user as U,  places as P, orders as O, contains as C, items as I WHERE U.user_id = P.user_id AND P.order_id = O.order_id AND C.order_id = O.order_id AND C.item_id = I.item_id';
+
+$r = mysqli_query($connection, $q);
+while ($row = mysqli_fetch_array ($r, MYSQLI_ASSOC)) {
+	echo '<tr>
+    <td align="center"><a href="view_order.php?order_id=' . $row['order_id'] . '">' . $row['order_id'] . '</a></td>
+	
+	<td align="center">' . $row['item_name'] .'</td>
+	<td align="center">' . $row['quantity'] .'</td>
+	
+    <td align="center">$' . $row['total_price'] .'</td>
+    <td align="right"><a href="view_customer.php?user_id=' . $row['user_id'] . '">' . htmlspecialchars( $row['name']) .'</a></td>
+    
+    <td align="center">' . $row['shipping_address'] .'</td>
+    <td align="center">' . $row['billing_address'] .'</td>
+    
+  </tr>';
+}
+
+echo '</tbody></table>';
+
+?>
+
+<script src="/js/jquery.dataTables.min.js" type="text/javascript" charset="utf-8"></script>
+<script type="text/javascript"> 
+// Enable Datatables:
+$(function() { 
+    $("#orders").dataTable();
+
+}); 
+</script>
