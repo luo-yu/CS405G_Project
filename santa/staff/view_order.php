450def2bfc58a9f5f9053c17b59075bc40c0d053
Yu Luo <luoy6@miamioh.edu>
2014-11-30 12:43:14 -0500

    Be able to view order

--- santa/staff/view_order.php
+++ santa/staff/view_order.php
@@ -1,6 +1,6 @@
 <?php
 
-// This file allows the administrator to view every order.
+// This file allows the administrator to view a single order.
 
 
 // Set the page title and include the header:
@@ -14,28 +14,32 @@ require("../../includes/db_connection.php");
 echo '<h3>View Orders</h3><table border="0" width="100%" cellspacing="4" cellpadding="4" id="orders">
 <thead>
 	<tr>
-	
+	<th align="center">Item ID</th>
     <th align="center">Order ID</th>
-	
+	<th align="center">Item Name</th>
+	<th align="center">Quantity</th>
     <th align="center">Total</th>
 
     <th align="center">Shipping Address</th>
 	<th align="center">Billing Address</th>
-	<th align="center">Ship this Order</a></th>
+	
   
   </tr></thead>
 <tbody>';
 
+$order_id = $_GET['id'];
+
 // Make the query:
-$q = 'SELECT O.order_id, O.total_price, O.order_date, O.shipping_address, O.billing_address FROM orders as O';
+$q = "SELECT I.item_id, I.item_name, C.quantity, O.order_id, O.total_price,U.name,O.order_date, O.shipping_address, O.billing_address FROM user as U,  places as P, orders as O, contains as C, items as I WHERE U.user_id = P.user_id AND P.order_id = O.order_id AND C.order_id = O.order_id AND C.item_id = I.item_id AND O.order_id = $order_id";
 
 $r = mysqli_query($connection, $q);
 while ($row = mysqli_fetch_array ($r, MYSQLI_ASSOC)) {
 	echo '<tr>
+	 <td align="center"><a href="view_order.php?item_id=' . $row['item_id'] . '">' . $row['item_id'] . '</a></td>
+    <td align="center"><a href="view_order.php?order_id=' . $row['order_id'] . '">' . $row['order_id'] . '</a></td>
 	
-    <td align="center"><a href="view_order.php?id=' . $row['order_id'] . '">' . $row['order_id'] . '</a></td>
-	
-	
+	<td align="center">' . $row['item_name'] .'</td>
+	<td align="center">' . $row['quantity'] .'</td>
 	
     <td align="center">$' . $row['total_price'] .'</td>
    
@@ -43,13 +47,14 @@ while ($row = mysqli_fetch_array ($r, MYSQLI_ASSOC)) {
     <td align="center">' . $row['shipping_address'] .'</td>
     <td align="center">' . $row['billing_address'] .'</td>
     
-	<td align="center"><a href="delete_record.php?action=delete&id=' . $row['order_id'] . '">Ship</a></td>
+	
   </tr>';
  
 }
 
 echo '</tbody></table>';
-
+//Delete row with item_id and order id from contains table, 
+//update item quantity in items table
 
 ?>
 
